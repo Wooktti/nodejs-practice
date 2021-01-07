341caf6d046dff5cbe97d45f7c3bcc131984bbdb
Wooktti <jaewooki1234@gmail.com>
2021-01-08 02:43:51 +0900

    readdir and construct li automatically

--- main.js
+++ main.js
@@ -11,61 +11,69 @@ var app = http.createServer(function(request,response){
 
     if(pathName === '/') {
         if (title === undefined) {
+            fs.readdir('./data', (error, filelist) => {
             title = 'Welcome';
             data = 'Hello, Node.js';
+            var list = '<ul>';
+
+            for (let i = 0; i < filelist.length; i++) {
+                list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+            }
+
+            list += '</ul>';
             template = `
-        <!doctype html>
-    <html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${data}</p>
-    </body>
-    </html>
-        `;
-            response.writeHead(200);
-            response.end(template);
-        } else {
-            fs.readFile(`data/${title}`, 'utf8', (err, data) => {
-                template = `
             <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${data}</p>
-        </body>
-        </html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${data}</p>
+            </body>
+            </html>
             `;
+
             response.writeHead(200);
             response.end(template);
             });
-        }
+            
+        } else {
+            fs.readdir('./data', (error, filelist) => {
+                var list = '<ul>';
+    
+                for (let i = 0; i < filelist.length; i++) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                }
+    
+                list += '</ul>';
+                fs.readFile(`data/${title}`, 'utf8', (err, data) => {
+                template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>${data}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+                });
+            });
+        } 
     } else {
         response.writeHead(404);
         response.end('Not found');
     }
-    
-    
-    
-    
 });
 app.listen(3000);
