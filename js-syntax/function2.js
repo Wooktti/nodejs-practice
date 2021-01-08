4610a0a43fb53ea729170621bb3d0516d939b9ec
Wooktti <jaewooki1234@gmail.com>
2021-01-08 11:48:12 +0900

    node js function

---
+++ js-syntax/function2.js
@@ -0,0 +1,13 @@
+console.log(Math.round(1.6)); // 2
+console.log(Math.round(1.4)); // 1
+
+function printSum(first, second) { // parameter
+    console.log(first + second);
+}
+
+function returnSum(first, second) {
+    return first + second;
+}
+
+printSum(2, 4); // argument
+console.log(returnSum(10, 12));
